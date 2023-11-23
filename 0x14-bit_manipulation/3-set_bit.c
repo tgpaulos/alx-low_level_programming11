6a4d8d8f35e322705ca91410404d0768121ffb44
tgpaulos <warsidotigist11@gmial.com>
2023-11-23 00:51:09 -0800

    commit all c files

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * set_bit - sets a bit at a given index to 1
+ * @n: pointer to the number to change
+ * @index: index of the bit to set to 1
+ * Return: 1 for success, -1 for error
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int mask = 1UL << index;
+
+	if (index >= (sizeof(unsigned long int) * 8))
+	{
+
+		return (-1);
+	}
+
+
+	*n |= mask;
+
+	return (1);
+}

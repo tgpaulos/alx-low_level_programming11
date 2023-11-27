31ddfbf29f15a617a44f92eebc899ee0fe214925
tgpaulos <warsidotigist11@gmial.com>
2023-11-27 11:43:43 -0800

    update in creat file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,7 +7,7 @@
  * Return: 1 on success, -1 on failure
  */
 
-vint create_file(const char *filename, char *text_content)
+int create_file(const char *filename, char *text_content)
 {
 	int length;
 	int fd;
